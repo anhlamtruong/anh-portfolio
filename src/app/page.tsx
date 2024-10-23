4f2b110026cb01e2ca68b4219b8c090f5179c204
BAU Registrar <registrar@bau.edu>
2024-10-23 17:38:09 -0400

    add homepage main screen add section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,10 @@
-import { ModeToggle } from "@/components/ui/theme-toggle";
+import { HomePageMainScreen } from "@/features/homepage/ui/homepage-main-screen";
 
 export default function Home() {
   return (
     <div>
       <main>
-        <div className="flex items-center justify-center">
-          <center className="  w-11/12 bg-red-400">
-            <ModeToggle></ModeToggle>
-            <section className=" h-screen">
-              <h1 className="text-7xl">Page1</h1>
-            </section>
-            <section>
-              <h1 className="text-7xl">Page2</h1>
-            </section>
-            <section>
-              <h1 className="text-7xl">Page3</h1>
-            </section>
-            <section>
-              <h1 className="text-7xl">Page4</h1>
-            </section>
-          </center>
-        </div>
+        <HomePageMainScreen></HomePageMainScreen>
       </main>
       {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer> */}
     </div>
