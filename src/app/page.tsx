3852a6d5046d898540661db3dcc638158e3482cb
BAU Registrar <registrar@bau.edu>
2024-10-23 16:15:36 -0400

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
-import { HomePageMainScreen } from "@/features/homepage/ui/homepage-main-screen";
+import { ModeToggle } from "@/components/ui/theme-toggle";
 
 export default function Home() {
   return (
     <div>
       <main>
-        <HomePageMainScreen></HomePageMainScreen>
+        <div className="flex items-center justify-center">
+          <center className="  w-11/12 bg-red-400">
+            <ModeToggle></ModeToggle>
+            <section className=" h-screen">
+              <h1 className="text-7xl">Page1</h1>
+            </section>
+            <section>
+              <h1 className="text-7xl">Page2</h1>
+            </section>
+            <section>
+              <h1 className="text-7xl">Page3</h1>
+            </section>
+            <section>
+              <h1 className="text-7xl">Page4</h1>
+            </section>
+          </center>
+        </div>
       </main>
       {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer> */}
     </div>
